465476c5017cddcfddfd713fd65941a9745d6636
Onwuka Chukwuebuka Nathan <onwukaebuka88@gmail.com>
2023-05-10 06:23:47 +0100

    Two Number Sum

--- Two_sum.py
+++ Two_sum.py
@@ -1,5 +1,5 @@
 """
-Two Number Sum
+Two Number Sum (Easy)
  Write a function that takes in a non-empty array of distinct integers and an integer
  representing a target sum. If any two numbers in the input array sum up to the target sum,
  the function should return them in an array, in any order. If no two numbers sum up to the
@@ -24,13 +24,16 @@ Two Number Sum
 """
 Pretty straightforward solution - store all values from array in a set. Then loop through each number in the array, looking for its pair in the set. If it's there we return the two numbers, otherwise loop exits and empty array is returned.
 """
+
+
 def twoNumberSum(array, targetSum):
     storage = set(num for num in array)
     for num in array:
         target = targetSum - num
         if target in storage and target is not num:
-             return [num, target]
+            return [num, target]
     return []
+
 
 array = [- 3, 5, -4, 8, 11, 1, -1, 6]
 targetSum = 10
